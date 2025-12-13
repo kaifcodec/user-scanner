1638c0dbd71cf5a744509119f3d1c3ee885e796b
Vamato <62358492+VamatoHD@users.noreply.github.com>
2025-12-13 13:10:15 +0000

    fix: converted indent into two spaces

--- user_scanner/cli/printer.py
+++ user_scanner/cli/printer.py
@@ -2,25 +2,30 @@ from colorama import Fore, Style
 from typing import Literal
 from user_scanner.core.result import Result, Status
 
+INDENT = "  "
+
 JSON_TEMPLATE = """{{
 \t"site_name": "{site_name}",
 \t"username": "{username}",
 \t"result": "{result}"
-}}"""
+}}""".replace("\t", INDENT)
 
 JSON_TEMPLATE_ERROR = """{{
 \t"site_name": "{site_name}",
 \t"username": "{username}",
 \t"result": "Error",
 \t"reason": "{reason}"
-}}"""
+}}""".replace("\t", INDENT)
+
 
 CSV_HEADER = "site_name,username,result,reason"
 CSV_TEMPLATE = "{site_name},{username},{result},{reason}"
 
 
-def identate(msg: str, ident: int):
-    tabs = "\t" * ident
+def indentate(msg: str, indent: int):
+    if indent <= 0:
+        return msg
+    tabs = INDENT * indent
     return "\n".join([f"{tabs}{line}" for line in msg.split("\n")])
 
 
@@ -29,7 +34,7 @@ class Printer:
         if not output_format in ["console", "csv", "json"]:
             raise ValueError(f"Invalid output-format: {output_format}")
         self.mode: str = output_format
-        self.ident: int = 0
+        self.indent: int = 0
 
     @property
     def is_console(self) -> bool:
@@ -45,16 +50,16 @@ class Printer:
 
     def print_start(self) -> None:
         if self.is_json:
-            self.ident += 1
-            print(identate("[", self.ident - 1))
+            self.indent += 1
+            print(indentate("[", self.indent - 1))
         elif self.is_csv:
             print(CSV_HEADER)
 
     def print_end(self) -> None:
         if not self.is_json:
             return
-        self.ident = max(self.ident - 1, 0)
-        print(identate("]", self.ident))
+        self.indent = max(self.indent - 1, 0)
+        print(indentate("]", self.indent))
 
     def get_result_output(self, site_name: str, username: str, result: Result) -> str:
         if result == None:
@@ -65,26 +70,26 @@ class Printer:
 
         match (result.status, self.mode):
             case (Status.AVAILABLE, "console"):
-                return f"  {Fore.GREEN}[✔] {site_name} ({username}): Available{Style.RESET_ALL}"
+                return f"{INDENT}{Fore.GREEN}[✔] {site_name} ({username}): Available{Style.RESET_ALL}"
 
             case (Status.TAKEN, "console"):
-                return f"  {Fore.RED}[✘] {site_name} ({username}): Taken{Style.RESET_ALL}"
+                return f"{INDENT}{Fore.RED}[✘] {site_name} ({username}): Taken{Style.RESET_ALL}"
 
             case (Status.ERROR, "console"):
                 reason = ""
                 if isinstance(result, Result) and result.has_reason():
                     reason = f" ({result.get_reason()})"
-                return f"  {Fore.YELLOW}[!] {site_name} ({username}): Error{reason}{Style.RESET_ALL}"
+                return f"{INDENT}{Fore.YELLOW}[!] {site_name} ({username}): Error{reason}{Style.RESET_ALL}"
 
             case (Status.AVAILABLE, "json") | (Status.TAKEN, "json"):
-                return identate(JSON_TEMPLATE, self.ident).format(
+                return indentate(JSON_TEMPLATE, self.indent).format(
                     site_name=site_name,
                     username=username,
                     result=str(result.status)
                 )
 
             case (Status.ERROR, "json"):
-                return identate(JSON_TEMPLATE_ERROR, self.ident).format(
+                return indentate(JSON_TEMPLATE_ERROR, self.indent).format(
                     site_name=site_name,
                     username=username,
                     reason=result.get_reason()
