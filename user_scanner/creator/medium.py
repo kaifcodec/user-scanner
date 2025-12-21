1ac0f20222dae71569fa77cfc703435dfe64bb7d
kaifcodec <kaifcodec@gmail.com>
2025-12-22 00:19:34 +0530

    fix: update the headers which fixes silent error for medium.com

--- user_scanner/creator/medium.py
+++ user_scanner/creator/medium.py
@@ -6,8 +6,19 @@ def validate_medium(user):
     url = f"https://medium.com/@{user}"
 
     headers = {
-        'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",
-        'Accept': "text/html",
+        'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        'Accept-Encoding': "identity",
+        'upgrade-insecure-requests': "1",
+        'sec-fetch-site': "none",
+        'sec-fetch-mode': "navigate",
+        'sec-fetch-user': "?1",
+        'sec-fetch-dest': "document",
+        'sec-ch-ua': "\"Google Chrome\";v=\"143\", \"Chromium\";v=\"143\", \"Not A(Brand\";v=\"24\"",
+        'sec-ch-ua-mobile': "?0",
+        'sec-ch-ua-platform': "\"Linux\"",
+        'accept-language': "en-US,en;q=0.9",
+        'priority': "u=0, i"
     }
 
     def process(response):
@@ -34,4 +45,4 @@ if __name__ == "__main__":
     elif result == 0:
         print("Unavailable!")
     else:
-        print("Error occurred!")
+        print(f"Error occurred! Reason: {result.get_reason()}")
