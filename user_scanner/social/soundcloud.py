c40612ce782db3aeef2ad339d709665b61bc81a5
kaifcodec <kaifcodec@gmail.com>
2025-12-22 08:21:51 +0530

    refactor: remove custom headers and use default headers

--- user_scanner/social/soundcloud.py
+++ user_scanner/social/soundcloud.py
@@ -5,11 +5,6 @@ from user_scanner.core.result import Result
 def validate_soundcloud(user):
     url = f"https://soundcloud.com/{user}"
 
-    headers = {
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    }
-
     def process(response):
         if response.status_code == 404:
             return Result.available()
@@ -28,7 +23,7 @@ def validate_soundcloud(user):
 
         return Result.error()
 
-    return generic_validate(url, process, headers=headers, follow_redirects=True)
+    return generic_validate(url, process, follow_redirects=True)
 
 
 if __name__ == "__main__":
