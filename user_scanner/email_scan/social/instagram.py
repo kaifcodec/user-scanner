c71001e388d6779e76bcf25409f8462a6a78e8b9
kaifcodec <kaifcodec@gmail.com>
2026-01-02 10:45:35 +0530

    refactor: changes made to work with email_orchestrator and asyncio

--- user_scanner/email_scan/social/instagram.py
+++ user_scanner/email_scan/social/instagram.py
@@ -1,9 +1,9 @@
-import asyncio
 import httpx
+from user_scanner.core.result import Result
 
-async def check_instagram(email):
-    ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-    async with httpx.AsyncClient(headers={"user-agent": ua}, http2=True) as client:
+async def _check(email):
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
+    async with httpx.AsyncClient(headers={"user-agent": USER_AGENT}, http2=True) as client:
         await client.get("https://www.instagram.com/")
         csrf = client.cookies.get("csrftoken")
 
@@ -14,27 +14,14 @@ async def check_instagram(email):
             "referer": "https://www.instagram.com/accounts/password/reset/"
         }
 
-        resp = await client.post(
+        response = await client.post(
             "https://www.instagram.com/api/v1/web/accounts/account_recovery_send_ajax/",
             data={"email_or_username": email},
             headers=headers
         )
 
-        data = resp.json()
-        if data.get("status") == "ok":
-            return f"[!] {email} -> USED (Account exists)"
-        return f"[*] {email} -> NOT USED"
+        data = response.json()
+        return Result.taken() if data.get("status") == "ok" else Result.available()
 
-if __name__ == "__main__":
-    target = input("Enter the Email: ")
-    print(asyncio.run(check_instagram(target)))
-
-
-
-
-
-
-
-
-
-
+def validate_instagram(email: str) -> Result:
+    return _check(email)
