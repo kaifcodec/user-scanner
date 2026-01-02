16ff86ff1b4e8c9d173f258ed270188e8d6415bd
kaifcodec <kaifcodec@gmail.com>
2026-01-02 10:33:44 +0530

    refactor: move helper functions to helpers.py

--- user_scanner/core/orchestrator.py
+++ user_scanner/core/orchestrator.py
@@ -1,67 +1,11 @@
-import importlib
-import importlib.util
 from colorama import Fore, Style
 from concurrent.futures import ThreadPoolExecutor
 from itertools import permutations
 import httpx
 from pathlib import Path
 from user_scanner.core.result import Result
-from types import ModuleType
-from typing import Callable, Dict, List
-from user_scanner.core.helpers import get_site_name
-
-
-def load_modules(category_path: Path) -> List[ModuleType]:
-    modules = []
-    for file in category_path.glob("*.py"):
-        if file.name == "__init__.py":
-            continue
-        spec = importlib.util.spec_from_file_location(file.stem, str(file))
-        if spec is None or spec.loader is None:
-            continue
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-
-        modules.append(module)
-    return modules
-
-
-def load_categories(is_email: bool = False) -> Dict[str, Path]:
-    folder_name = "email_scan" if is_email else "user_scan"
-    root = Path(__file__).resolve().parent.parent / folder_name
-    categories = {}
-
-    for subfolder in root.iterdir():
-        if subfolder.is_dir() and \
-                subfolder.name.lower() not in ["cli", "utils", "core"] and \
-                "__" not in subfolder.name:  # Removes __pycache__
-            categories[subfolder.name] = subfolder.resolve()
-
-    return categories
-
-
-def find_module(name: str, is_email: bool = False) -> List[ModuleType]:
-    name = name.lower()
-
-    return [
-        module
-        for category_path in load_categories(is_email).values()
-        for module in load_modules(category_path)
-        if module.__name__.split(".")[-1].lower() == name
-    ]
-
-
-def find_category(module: ModuleType) -> str | None:
-
-    module_file = getattr(module, '__file__', None)
-    if not module_file:
-        return None
-
-    category = Path(module_file).parent.name.lower()
-    if category in load_categories(False) or category in load_categories(True):
-        return category.capitalize()
-
-    return None
+from typing import Callable, List
+from user_scanner.core.helpers import find_category,  get_site_name, load_categories, load_modules
 
 
 def worker_single(module, username: str) -> Result:
