52ad6496ddf6c6bf6695d6070a63759f07ab86f1
Vladyslav <50027516+notlorick@users.noreply.github.com>
2026-05-08 23:06:19 +0300

    fixed vk

--- user_scanner/user_scan/social/vk.py
+++ user_scanner/user_scan/social/vk.py
@@ -1,8 +1,16 @@
-from user_scanner.core.orchestrator import status_validate
+from user_scanner.core.orchestrator import generic_validate, Result
 
 
 def validate_vk(user):
-    url = f"https://www.vk.com/{user}"
-    show_url = f"https://www.vk.com/{user}"
+    url = f"https://vk.com/{user}"
+    show_url = f"https://vk.com/{user}" 
 
-    return status_validate(url, 404, 200, show_url=show_url)
+    def process(response):
+        if response.status_code == 404:
+            return Result.available()
+        elif response.status_code == 200:
+            return Result.taken()
+
+        return Result.error("Unexpected response body, report it via GitHub issues.")
+
+    return generic_validate(url, process, show_url=show_url)
