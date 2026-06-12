a0491c023fe82f7368a8197fe0e17a3b65ff25fd
Kaif <kaifcodec@gmail.com>
2026-06-12 23:21:09 +0530

    add: added 18 new user_scan modules

---
+++ user_scanner/user_scan/social/goodreads.py
@@ -0,0 +1,25 @@
+import re
+from user_scanner.core.helpers import get_random_user_agent
+from user_scanner.core.orchestrator import Result, make_request
+
+def validate_goodreads(user):
+    url = f"https://www.goodreads.com/{user}"
+    
+    headers = {
+        "User-Agent": get_random_user_agent(),
+    }
+    
+    resp = make_request(url, headers=headers, http2=True)
+    if resp.status_code == 404:
+        return Result.available(url=url)
+    elif resp.status_code == 200:
+        extra = {}
+        title_match = re.search(r'<title>(.*?)</title>', resp.text, re.IGNORECASE)
+        if title_match:
+            extra["title"] = title_match.group(1).strip()
+        name_match = re.search(r'<meta[^>]*property=["\']og:title["\'][^>]*content=["\']([^"\']+)["\']', resp.text, re.IGNORECASE)
+        if name_match:
+            extra["name"] = name_match.group(1).replace(' (', '').strip()
+        return Result.taken(extra=extra, url=url)
+        
+    raise Exception(f"Unexpected status code {resp.status_code}")
