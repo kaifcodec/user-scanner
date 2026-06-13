365fedfd2c2e0b37194de76469eacefc99fefe92
Boxc4t <lukas.cat099099@gmail.com>
2026-06-13 12:45:28 +0300

    Added a basic nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "user-scanner";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.python312Packages.buildPythonApplication {
+        pname = "user-scanner";
+        version = "1.4.0.1";
+
+        src = self;
+
+        pyproject = true;
+
+        build-system = with pkgs.python312Packages; [
+          flit-core
+        ];
+
+        dependencies = with pkgs.python312Packages; [
+          httpx
+          socksio
+          colorama
+          h2
+        ];
+
+        pythonImportsCheck = ["user_scanner"];
+      };
+    });
+
+    apps = forAllSystems (system: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/user-scanner";
+      };
+    });
+  };
+}
