3a7100c57a3b356912f04cc6b2f7e2751f660872
kaifcodec <kaifcodec@gmail.com>
2026-07-21 09:34:39 +0530

    chore: bump version to 1.4.1.9

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       in {
         default = pkgs.python312Packages.buildPythonApplication {
           pname = "user-scanner";
-          version = "1.4.1.8";
+          version = "1.4.1.9";
 
           src = self;
 
